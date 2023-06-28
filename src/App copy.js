d463e7248163dbe0f068a5f367cf067784c661a6
Heechem <hichempeter@gmail.com>
2023-06-28 07:52:36 -0600

    Adding App title

--- src/App copy.js
+++ src/App copy.js
@@ -318,6 +318,12 @@ const SelectedMovie = ({ selectedId, onCloseMovie, onAddWatched, watched }) => {
     },
     [selectedId]
   );
+
+  useEffect(() => {
+    if (!title) return;
+    document.title = `Movie | ${title}`;
+  }, [title]);
+
   return (
     <div className="details">
       {isLoading ? (
